228ad3e9416a1e619b6f5b0ba69cb4ba3deea964
davim5 <davim53@hotmail.com>
2023-11-30 18:24:00 -0300

    Implementando método adicionado pelo outro integrante

--- src/main.java
+++ src/main.java
@@ -3,7 +3,14 @@ public class main {
         // 1° Chamada, Informações do carro
 
         System.out.println("--- Criando personagem ---");
-        Carro Player1 = new Personagem("Davi", "Guerreiro", 1, 10, 10, 10, 100);
+        Personagem Player1 = new Personagem("Davi", "Guerreiro", 1, 10, 10, 10, 100);
         Player1.toString();
+
+        System.out.println("--- Criando personagem 2 ---");
+        Personagem Player2 = new Personagem("João", "Mago", 1, 10, 10, 10, 60);
+        Player2.toString();
+
+        // Método de atacar
+        Player1.atacar(Player2);
     }
 }
