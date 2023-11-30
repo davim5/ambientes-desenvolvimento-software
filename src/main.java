f2905b1114a5c784b6104b02c0742b50f3d48a0a
davim5 <davim53@hotmail.com>
2023-11-30 18:12:54 -0300

    Criando personagem: Integrante 1

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        // 1° Chamada, Informações do carro
+
+        System.out.println("--- Criando personagem ---");
+        Carro Player1 = new Personagem("Davi", "Guerreiro", 1, 10, 10, 10, 100);
+        Player1.toString();
+    }
+}
