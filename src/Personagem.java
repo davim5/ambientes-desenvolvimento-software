cd768ff18501e6168a228bbefd56a1be8326a357
davim5 <davim53@hotmail.com>
2023-11-30 17:29:19 -0300

    Firts commit: integrante 1

---
+++ src/Personagem.java
@@ -0,0 +1,72 @@
+public class Personagem {
+    private String nome;
+    private String classe;
+    private int level;
+    private int força;
+    private int velocidade;
+    private int inteligencia;
+    private int pontosDeVida;
+
+    public Personagem(String nome, String classe, int level, int força, int velocidade, int inteligencia, int pontosDeVida) {
+        this.nome = nome;
+        this.classe = classe;
+        this.level = level;
+        this.força = força;
+        this.velocidade = velocidade;
+        this.inteligencia = inteligencia;
+        this.pontosDeVida = pontosDeVida;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setClasse(String classe) {
+        this.classe = classe;
+    }
+
+    public String getClasse() {
+        return classe;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setForça(int força) {
+        this.força = força;
+    }
+
+    public int getForça() {
+        return força;
+    }
+
+    public void setVelocidade(int velocidade) {
+        this.velocidade = velocidade;
+    }
+
+    public int getVelocidade() {
+        return velocidade;
+    }
+
+    public void setInteligencia(int inteligencia) {
+        this.inteligencia = inteligencia;
+    }
+
+    public int getInteligencia() {
+        return inteligencia;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome: " + nome + "\nClasse: " + classe + "\nLevel: " + level + "\nForça: " + força + "\nVelocidade: " + velocidade + "\nInteligência: " + inteligencia;
+    }
+}
